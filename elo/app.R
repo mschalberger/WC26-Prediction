808ceda35e695dad8b86003320d58fb5236aeac5
Ulrich C. Schneider <qomanaut@gmail.com>
2026-05-27 19:58:17 +0200

    Reduce workers on production server from 12 to 3 for app instances

--- elo/app.R
+++ elo/app.R
@@ -21,7 +21,7 @@ n_workers <- local({
   if (nzchar(env_val)) {
     as.integer(env_val)
   } else if (dir.exists("/srv/shiny-server")) {
-    12L   # Produktions-Server
+    3L   # Produktions-Server (4 App-Instanzen x 3 Worker = 12)
   } else {
     max(1L, parallel::detectCores() - 1L)   # Lokale Entwicklung
   }
